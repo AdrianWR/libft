74225e4ee85248d54560a53758c2a3f7551b12b3
Adrian W. Roque <adrian.w.roque@gmail.com>
2020-01-26 02:08:48 -0300

    Update minunit.h

--- test/minunit.h
+++ test/minunit.h
@@ -1,14 +1,8 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minunit.h                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aroque <marvin@42.fr>                      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/21 14:34:35 by aroque            #+#    #+#             */
-/*   Updated: 2020/01/23 21:27:04 by aroque           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/*
+** This unit test framework can be found at
+** http://www.jera.com/techinfo/jtns/jtn002.html
+** All credits to its creator.
+*/
 
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
 #define mu_run_test(test) do { char *message = test(); tests_run++; if (message) return message; } while (0)
