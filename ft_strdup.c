0c4d315b82c9f27a69c6c871e60b3c880ebf07c7
Adrian W. Roque <adrian.w.roque@gmail.com>
2020-02-12 10:33:23 -0300

    Update ft_strdup.c
    Add correction for NULL input.

--- ft_strdup.c
+++ ft_strdup.c
@@ -18,7 +18,7 @@ char	*ft_strdup(const char *s1)
 	char	*s;
 
 	i = -1;
-	if (!(s = malloc((ft_strlen(s1) + 1) * sizeof(*s))))
+	if (!s1 || !(s = malloc((ft_strlen(s1) + 1) * sizeof(*s))))
 		return (NULL);
 	while (s1[++i])
 		s[i] = s1[i];
