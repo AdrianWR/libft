69921f7204ef778aac8dbd531551838ea3edfdda
Adrian W. Roque <adrian.w.roque@gmail.com>
2020-09-17 22:19:10 -0300

    Remove duplicated prototype

--- libft.h
+++ libft.h
@@ -23,7 +23,6 @@
 
 void				ft_bzero(void *s, size_t n);
 void				*ft_memset(void *b, int c, size_t len);
-int					ft_atoi(const char *str);
 void				*ft_memcpy(void *dst, const void *src, size_t n);
 void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
